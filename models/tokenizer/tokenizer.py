8e83248dd387d38a7adc39b0435a2afed7600769
gashon <gashon96@gmail.com>
2024-04-09 01:38:45 -0700

    feat: tknizer without special tkns

--- models/tokenizer/tokenizer.py
+++ models/tokenizer/tokenizer.py
@@ -33,69 +33,63 @@ class Tokenizer:
         merges = [(bytes(a, 'utf-8'), bytes(b, 'utf-8')) for a, b in merges]
         return cls(vocab, merges, special_tokens)
 
-    def pretokenize(self, text: str) -> Dict[str, int]:
-        match_count = {} 
+    def pretokenize(self, text: str) -> List[str]:
+        matches = [] 
 
         for match in self.PAT.finditer(text):
             match_str = match.group(0)
             if match_str in self.special_tokens:
                 continue
-            match_count[match_str] = match_count.get(match_str, 0) + 1
+            matches.append(match_str)
 
-        return match_count
+        return matches 
 
-    def merge(self, arr: List[bytes]):
-        pairs = [pair for pair in zip(arr, arr[1:])] 
-        while len(arr) >= 2:
-            to_merge_pair = min(pairs, key=lambda p: self.merges.get(p, float("inf")))
-            if to_merge_pair not in self.merges:
-                break
-            for i in range(len(arr) - 1):
-                if arr[i:i+2] == list(to_merge_pair):
-                    arr[i:i+2] = [to_merge_pair[0] + to_merge_pair[1]]
-                    i -= 1
-                    
-            pairs = [pair for pair in zip(arr, arr[1:])]
+    def merge(self, tokens: List[bytes], pair: Tuple[bytes, bytes], replacement: bytes) -> List[bytes]:
+        new_tokens = []
+        i = 0
+        while i < len(tokens):
+            if tokens[i] == pair[0] and i < len(tokens) - 1 and tokens[i + 1] == pair[1]:
+                new_tokens.append(replacement)
+                i += 2
+            else:
+                new_tokens.append(tokens[i])
+                i += 1
+
+        return new_tokens
 
     def encode(self, text: str) -> List[int]:
         # Pre-tokenize
         pre_token_count = self.pretokenize(text)
-        encoded_tokens = []
+        print("Pre-token count: ", pre_token_count)
+        
+        ids = []
+        for token in pre_token_count:
+            raw_bytes = [bytes([b]) for b in token.encode('utf-8')]
+            token_ids = []
 
-        for token in pre_token_count.keys():
+            while len(raw_bytes) > 1:
+                pairs = [pair for pair in zip(raw_bytes, raw_bytes[1:])]
+                pair = min(pairs, key= lambda p: self.merges.get(p, float("inf")))
+                if pair not in self.merges:
+                    break
+                raw_bytes = self.merge(raw_bytes, pair, pair[0] + pair[1])
+            for byte in raw_bytes:
+                token_ids.append(self.vocab_inv[byte])
+            ids.extend(token_ids)
 
-            byte_arr = [bytes(char, 'utf-8') for char in token]
-            # convert to byte_array
-            subwords = []
-            for byte_str in byte_arr:
-                for byte in byte_str:
-                    subwords.append(bytes([byte]))
-
-            self.merge(subwords)
-            encoded = []
-            for byte in subwords:
-                if byte in self.vocab_inv:
-                    encoded.append(self.vocab_inv[byte])
-                else:
-                    # add to vocab and vocab_inv
-                    self.vocab[len(self.vocab)] = byte 
-                    self.vocab_inv[byte] = len(self.vocab) - 1
-                    encoded.append(self.vocab_inv[byte])
-                    # encoded.append(self.vocab_inv['<unk>'.encode('utf-8')])
-
-            encoded_freq = pre_token_count[token]
-            encoded_tokens.extend(encoded * encoded_freq)
-
-        return encoded_tokens
+        return ids
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         for text in iterable:
             yield from self.encode(text)
 
     def decode(self, ids: List[int]) -> str:
-        if len(ids) == 0:
-            return ''
-        byte_agr = self.vocab[ids[0]]
-        for id in ids[1:]:
-            byte_agr = byte_agr + self.vocab[id]
-        return byte_agr.decode('utf-8')
+        raw_bytes = b"".join([self.vocab[i] for i in ids])
+        return raw_bytes.decode('utf-8', errors='replace')
+
+    def _get_stats(self, tokens: List[bytes]) -> Dict[Tuple[bytes, bytes], int]:
+        counts = collections.defaultdict(int)
+        for pair in zip(tokens, tokens[1:]):
+            counts[pair] += 1
+        return counts
+
